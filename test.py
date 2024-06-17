bdddd570a2cd1c852935624d4e9e8f6cf74e0080
sergistenz <152603011+sergistenz@users.noreply.github.com>
2024-06-17 17:40:35 +0100

    Updated gitignore, deleted test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-import os
-from dotenv import load_dotenv
-import pandas as pd
-from moviefinder import genres_list
-from moviefinder import get_movie_dataset
-
-
-get_movie_dataset()
